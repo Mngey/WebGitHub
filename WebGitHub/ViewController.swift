8c3271e9cab62743362e97e8f806d1abc91e6262
mngey <mitrofanova92@gmail.com>
2020-09-11 19:46:36 +0300

    Initial Commit

---
+++ WebGitHub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebGitHub
+//
+//  Created by a.a.mitrofanova on 11/09/2020.
+//  Copyright © 2020 mngey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
